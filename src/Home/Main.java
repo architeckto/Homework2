1981f1cd1d46caf83d38a5e84421cd8067a39721
Architeckto <tserulnikov.p@gmail.com>
2022-06-12 00:18:57 +0700

    Правки

--- src/Home/Main.java
+++ src/Home/Main.java
@@ -1,9 +1,10 @@
 package Home;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
+        counting counting = new counting();
         String[][] car = new String[4][4];
-        jeep();
-        calculation;
+        counting.jeep(car);
+        counting.calculation(car);
     }
 }
